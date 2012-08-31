ed02c80beab67a25d4bf29103385011e2e92683a
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2012-08-31 21:20:39 +0530

    signal: add integration test with event bus
    I couldn't find anyway to test whether the callback takes place or
    not. I am relying on tests for the cyrusbus module to be stringent
    enough.
    
    For now, only testing whether the callback got the arguments as
    expected.

--- spiking/tests/signal_tests.py
+++ spiking/tests/signal_tests.py
@@ -1,5 +1,7 @@
-from nose.tools import eq_
 from datetime import datetime
+
+from nose.tools import *
+from cyrusbus import Bus
 
 from spiking.signal import TimedSignal
 
@@ -12,3 +14,21 @@ def test_timed_signal():
   # Check whether same values are maintained
   eq_(a.time, time, "Expected same time value!")
   eq_(a.neuron, '<1,2>', "Expected same neuron index!")
+
+def test_timed_signal_with_eventbus():
+  time = datetime.now()
+
+  # Create a signal
+  a = TimedSignal('<1,2>', time)
+
+  b = Bus()
+  
+  def callback(bus, signal=None):
+    eq_(signal.time, time, "Expected same time value!")
+    eq_(signal.neuron, '<1,2>', "Expected same neuron index!")
+
+  b.subscribe('Fired', callback)
+
+  # Fire the signal
+  b.publish('Fired', signal=a)
+
